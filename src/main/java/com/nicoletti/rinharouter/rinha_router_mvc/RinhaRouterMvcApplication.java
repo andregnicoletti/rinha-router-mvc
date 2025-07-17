4f9eb8faa7355c4602c144f011daf451c03836b9
André Nicoletti <andregnicoletti@gmail.com>
2025-07-17 13:40:28 -0300

    first commit

---
+++ src/main/java/com/nicoletti/rinharouter/rinha_router_mvc/RinhaRouterMvcApplication.java
@@ -0,0 +1,13 @@
+package com.nicoletti.rinharouter.rinha_router_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinhaRouterMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinhaRouterMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicoletti/rinharouter/rinha_router_mvc/RinhaRouterMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicoletti.rinharouter.rinha_router_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaRouterMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
